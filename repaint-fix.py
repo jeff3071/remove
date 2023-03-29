23a2849f37ebf7c132da4a449b541755b46ee08d
jeff3071 <jeff563715@gmail.com>
2023-03-29 10:57:02 +0800

    resize

--- repaint-fix.py
+++ repaint-fix.py
@@ -28,6 +28,16 @@ def repaint_image(image: Image, mask: Image) -> Image:
     img_array = np.array(response_json['image']).astype(np.uint8)
     return Image.fromarray(img_array).convert('RGB')
 
+
+def image_resize(image, image_height, image_width):
+    max_width = 720
+    max_height = 720
+    if image_width / image_height > max_width / max_height:
+        new_image = image.resize((max_width, int(image_height * max_width / image_width)))
+    else:
+        new_image = image.resize((int(image_width * max_height / image_height), max_height))
+    return new_image
+
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 bg_image = st.file_uploader("Background image:", type=["png", "jpg", "jpeg"])
 st.markdown("\n")
@@ -36,22 +46,24 @@ col1, col2 = st.columns(2)
 if bg_image is not None:
     image_upload = Image.open(bg_image)
     image = np.array(image_upload)
-
+    
+    resize_image = image_resize(image_upload, image.shape[0], image.shape[1])
+    new_image_size = np.array(resize_image)
     with col1:
         canvas_result = st_canvas(
             fill_color="rgba(255, 165, 0, 0.3)",
             stroke_width=35,
             stroke_color="#e9ec1290",
-            background_image=image_upload,
+            background_image=resize_image,
             update_streamlit=True,
             drawing_mode="freedraw",
-            height = image.shape[0],
-            width = image.shape[1],
+            height = new_image_size.shape[0],
+            width = new_image_size.shape[1],
         )
 
     with col2:
         if canvas_result.image_data is not None and np.any(canvas_result.image_data):
-            # st.image(image_upload)
+            # st.image(resize_image)
 
             mask = Image.fromarray(canvas_result.image_data.astype(np.uint8)*255)
 
