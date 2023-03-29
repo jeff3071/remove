dd14883a26dc57d59632da5e5bfe61aa2ae151ad
jeff3071 <jeff563715@gmail.com>
2023-03-29 12:49:25 +0800

    解決reload

--- repaint-fix.py
+++ repaint-fix.py
@@ -28,15 +28,15 @@ def repaint_image(image: Image, mask: Image) -> Image:
     img_array = np.array(response_json['image']).astype(np.uint8)
     return Image.fromarray(img_array).convert('RGB')
 
-
-def image_resize(image, image_height, image_width):
+def image_resize(_image, image_height, image_width):
     max_width = 720
     max_height = 720
     if image_width / image_height > max_width / max_height:
-        new_image = image.resize((max_width, int(image_height * max_width / image_width)))
+        new_image = _image.resize((max_width, int(image_height * max_width / image_width)))
     else:
-        new_image = image.resize((int(image_width * max_height / image_height), max_height))
+        new_image = _image.resize((int(image_width * max_height / image_height), max_height))
     return new_image
+
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 bg_image = st.file_uploader("Background image:", type=["png", "jpg", "jpeg"])
@@ -47,29 +47,40 @@ if bg_image is not None:
     image_upload = Image.open(bg_image)
     image = np.array(image_upload)
     
+    toolbox_container = st.container()
+    with toolbox_container:
+        toolbox_col1, toolbox_col2, toolbox_col3 = st.columns(3)
+        with toolbox_col2:
+            stroke_width = st.slider("Stroke width: ", 1, 100, 35)
+    
+
     resize_image = image_resize(image_upload, image.shape[0], image.shape[1])
     new_image_size = np.array(resize_image)
+    
     with col1:
         canvas_result = st_canvas(
             fill_color="rgba(255, 165, 0, 0.3)",
-            stroke_width=35,
+            stroke_width=stroke_width,
             stroke_color="#e9ec1290",
             background_image=resize_image,
             update_streamlit=True,
             drawing_mode="freedraw",
             height = new_image_size.shape[0],
             width = new_image_size.shape[1],
+            key="canvas",
         )
-
+        # st.download_button(
+        #     "send image", convert_image(resize_image), "fixed.png", "image/png"
+        # )
     with col2:
         if canvas_result.image_data is not None and np.any(canvas_result.image_data):
-            # st.image(resize_image)
+            st.image(resize_image)
 
-            mask = Image.fromarray(canvas_result.image_data.astype(np.uint8)*255)
+            # mask = Image.fromarray(canvas_result.image_data.astype(np.uint8)*255)
 
-            result_img = repaint_image(image_upload, mask)
+            # result_img = repaint_image(image_upload, mask)
 
-            ste.download_button(
-                "Download fixed image", convert_image(result_img), "fixed.png", "image/png"
+            st.download_button(
+                "Download fixed image", convert_image(resize_image), "fixed.png", "image/png"
             )
-            st.image(result_img)
+            # st.image(result_img)
